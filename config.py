17c8bfbd504bd6d2feed91d2294eb2f104d23795
pkbotzs <165777512+pkbotzs@users.noreply.github.com>
2024-09-28 12:48:16 -0700

    Update config.py

--- config.py
+++ config.py
@@ -23,30 +23,18 @@ BOT_STATS_TEXT = os.environ.get("BOTS_STATS_TEXT","<b>BOT UPTIME 🌺</b>\n{upti
 #send custom message when user interact with bot
 USER_REPLY_TEXT = os.environ.get("USER_REPLY_TEXT", "Don't send me messages directly I'm only File Share bot! 📌")
 
-#your bot token here from https://telegram.me/BotFather
 TG_BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", "") 
-#your api id from https://my.telegram.org/apps
 APP_ID = int(os.environ.get("APP_ID", ""))
-#your api hash from https://my.telegram.org/apps
 API_HASH = os.environ.get("API_HASH", "")
-#your channel_id from https://t.me/MissRose_bot by forwarding dummy message to rose and applying command `/id` in reply to that message
-CHANNEL_ID = int(os.environ.get("CHANNEL_ID", "-1001996959044"))
-#your id of telegram can be found by https://t.me/MissRose_bot with '/id' command
+CHANNEL_ID = int(os.environ.get("CHANNEL_ID", "-1002490172926"))
 OWNER_ID = int(os.environ.get("OWNER_ID", "5709370072"))
-#port set to default 8080
 PORT = os.environ.get("PORT", "8080")
-#your database url mongodb only You can use mongo atlas free cloud database
 DB_URL = os.environ.get("DB_URL", "mongodb+srv://hostingupfilterbot:L9SWitkjhygWHtzb@hostingupfilterbot.yud3x.mongodb.net/?retryWrites=true&w=majority&appName=hostingupfilterbot")
-#your database name
 DB_NAME = os.environ.get("DB_NAME", "hostingupfilterbot")
 
-#for creating telegram thread for bot to improve performance of the bot
 TG_BOT_WORKERS = int(os.environ.get("TG_BOT_WORKERS", "60"))
-#your start default command message.
 START_MSG = os.environ.get("START_MESSAGE", "Hello {first}\n\nI can store private files in Specified Channel and other users can access it from special link. 💾")
-#your telegram tag without @
 OWNER_TAG = os.environ.get("OWNER_TAG", "5709370072")
-#Time in seconds for message delete, put 0 to never delete
 TIME = int(os.environ.get("TIME", "1060"))
 
 
@@ -55,16 +43,9 @@ TIME = int(os.environ.get("TIME", "1060"))
 some token verification sites
 https://dashboard.shareus.io/
 """
-
-# Turn this feature on or off using True or False put value inside  ""
-# TRUE for yes FALSE if no 
 USE_SHORTLINK = True if os.environ.get('USE_SHORTLINK', "TRUE") == "TRUE" else False 
-# only shareus service known rightnow rest you can test on your own
 SHORTLINK_API_URL = os.environ.get("modijiurl.com")
-# SHORTLINK_API_KEY = os.environ.get("SHORTLINK_API_KEY", "6a0a4f826e12f701a433063ebbe730caa1c29c38")
-#use this key if not working ☠️ (jokin!!)
 SHORTLINK_API_KEY = os.environ.get("SHORTLINK_API_KEY", "6a0a4f826e12f701a433063ebbe730caa1c29c38")
-#add your custom time in secs for shortlink expiration.
 # 24hr = 86400
 # 12hr = 43200
 VERIFY_EXPIRE = int(os.environ.get('VERIFY_EXPIRE', "43200")) # Add time in seconds
@@ -83,7 +64,7 @@ USE_PAYMENT = True if (True if os.environ.get("USE_PAYMENT", "FALSE") == "TRUE" 
 #UPI ID
 UPI_ID = os.environ.get("UPI_ID", " ")
 #UPI QR CODE IMAGE
-UPI_IMAGE_URL = os.environ.get("UPI_IMAGE_URL", "")
+UPI_IMAGE_URL = os.environ.get("UPI_IMAGE_URL", "https://telegra.ph/file/8a6d4ed449d76c394d8be.jpg")
 #SCREENSHOT URL of ADMIN for verification of payments
 SCREENSHOT_URL = os.environ.get("SCREENSHOT_URL", f"t.me/{OWNER_TAG}")
 #Time and its price
@@ -101,23 +82,11 @@ PRICE5 = os.environ.get("PRICE5", "999 rs")
 
 
 #force message for joining the channel
-FORCE_MSG = os.environ.get("FORCE_MSG", "Hello {first}\n\n<b>You need to join in my Channel/Group to use me\n\nKindly Please join Channel</b> 🥺")
-#custom caption 
+FORCE_MSG = os.environ.get("FORCE_MSG", "Hello {first}\n\n<b>You need to join in my Channel/Group to use me\n\nKindly Please join Channel</b> 🥺") 
 CUSTOM_CAPTION = os.environ.get("CUSTOM_CAPTION", None)
-#protected content so that no files can be sent from the bot to anyone. recommended False
-# TRUE for yes FALSE if no
 PROTECT_CONTENT = True if os.environ.get("PROTECT_CONTENT", "FALSE") == "TRUE" else False
-#used if you dont need buttons on database channel.
-# True for yes False if no
 DISABLE_CHANNEL_BUTTON = True if os.environ.get("DISABLE_CHANNEL_BUTTON", "TRUE") == "TRUE" else False
-#you can add admin inside the bot(bug right now will fix later)
-
-#add admins with space seperated
-# 7195990000 289371935 248979023
-ADMIN_LIST = os.environ.get("ADMINS", "").split()
-
-
-
+ADMIN_LIST = os.environ.get("ADMINS", "5709370072").split()
 
 #no need to add anything from now on
 ADMINS = [int(admin) for admin in ADMIN_LIST if admin.isdigit()]
