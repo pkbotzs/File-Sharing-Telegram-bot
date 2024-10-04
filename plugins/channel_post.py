48d7bf482d0068fe5b8ca86baa3f010c93269872
AnshVachhani <142148746+AnshVachhani@users.noreply.github.com>
2024-10-04 23:30:41 +0530

    Update channel_post.py

--- plugins/channel_post.py
+++ plugins/channel_post.py
@@ -9,16 +9,17 @@ from helper_func import encode
 
 @Bot.on_message(filters.private & filters.user(ADMINS) & ~filters.command(['start','users','broadcast','batch','genlink','stats','auth_secret','deauth_secret', 'auth', 'sbatch', 'exit', 'add_admin', 'del_admin', 'admins', 'add_prem', 'ping', 'restart', 'ch2l', 'cancel']))
 async def channel_post(client: Client, message: Message):
-    reply_text = await message.reply_text("Please Wait...! 🫷", quote = True)
+    reply_text = await message.reply_text("Please Wait...! 🫷", quote=True)
     try:
-        post_message = await message.copy(chat_id = client.db_channel.id, disable_notification=True)
+        post_message = await message.copy(chat_id=client.db_channel.id, disable_notification=True)
     except FloodWait as e:
         await asyncio.sleep(e.value)
-        post_message = await message.copy(chat_id = client.db_channel.id, disable_notification=True)
+        post_message = await message.copy(chat_id=client.db_channel.id, disable_notification=True)
     except Exception as e:
         print(e)
         await reply_text.edit_text("Something went Wrong..!")
         return
+
     converted_id = post_message.id * abs(client.db_channel.id)
     string = f"get-{converted_id}"
     base64_string = await encode(string)
@@ -26,7 +27,11 @@ async def channel_post(client: Client, message: Message):
 
     reply_markup = InlineKeyboardMarkup([[InlineKeyboardButton("🔁 Sʜᴀʀᴇ ᴜʀʟ", url=f'https://telegram.me/share/url?url={link}')]])
 
-    await reply_text.edit(f"<b>🧑‍💻 ʜᴇʀᴇ ɪs ʏᴏᴜʀ ᴄᴏᴅᴇ : \n<code>{base64_string}</code></b>\n\n<b>🔗 ʜᴇʀᴇ ɪs ʏᴏᴜʀ ʟɪɴᴋ 👇:</b>\n{link}", reply_markup=reply_markup, disable_web_page_preview = True)
+    new_text = f"<b>🧑‍💻 ʜᴇʀᴇ ɪs ʏᴏᴜʀ ᴄᴏᴅᴇ : \n<code>{base64_string}</code></b>\n\n<b>🔗 ʜᴇʀᴇ ɪs ʏᴏᴜʀ ʟɪɴᴋ 👇:</b>\n{link}"
+
+    # Check if the current message text is the same as the new text
+    if reply_text.text != new_text:
+        await reply_text.edit(new_text, reply_markup=reply_markup, disable_web_page_preview=True)
 
     if not DISABLE_CHANNEL_BUTTON:
         try:
