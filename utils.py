9d3dfe1738e66d4a9bd19c3a622d7a59af1a2307
AnshVachhani <142148746+AnshVachhani@users.noreply.github.com>
2024-10-04 23:20:42 +0530

    Update utils.py

--- utils.py
+++ utils.py
@@ -1,20 +1,23 @@
 #anshvachhani
-from shortzy import Shortzy  # Import your link shortening class
+from shortzy import Shortzy
 
 
+SHORTNER_MODE = True 
+SHORTNER_URL = "https://modijiurl.com"
+SHORTNER_API = "6a0a4f826e12f701a433063ebbe730caa1c29c38"
 
 async def short_link(link):
-    if not SHORTNER_MODE:  # Skip shortening if mode is off
+    if not SHORTNER_MODE:
         return link
 
-    # Ensure API key and base URL are available
+  
     api_key = SHORTNER_API
     base_site = SHORTNER_URL
 
     if not (api_key and base_site):
-        return link  # Return original link if API key or URL is missing
+        return link
 
-    shortzy = Shortzy(api_key, base_site)  # Initialize shortener
-    short_link = await shortzy.convert(link)  # Shorten the link
+    shortzy = Shortzy(api_key, base_site)
+    short_link = await shortzy.convert(link)  
 
     return short_link
